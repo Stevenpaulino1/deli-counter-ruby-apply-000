014207e8e11313f368575e0622da520f23fc6b24
Stevenpaulino1 <stevenpaulino1@hotmail.com>
2018-06-27 13:49:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,7 @@ def take_a_number(line, name)
   line<<(name)
   puts "Welcome, #{name}. You are number #{line.length} in line."
 end
+
 def now_serving(line)
   if line != []
     puts "Currently serving #{line.shift}."
